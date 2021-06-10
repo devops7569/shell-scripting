080a68d11446e7dab7195c9fed0c9f1860c23464
devops7569 <devopstraining7569@gmail.com>
2021-06-09 23:09:43 -0700

    adding colors and tabs

--- print.sh
+++ print.sh
@@ -4,6 +4,10 @@ echo "hello world"
 echo "first shell scripting"
 echo "for devops training"
 
+
 echo -e "Hai \n this is venkatesh"
 echo -e "venkatesh \n mounika \n saanvitha"
 
+echo -e "venkatesh \t \t mounika \n saanvitha"
+
+echo -e "\e[31mplease check the errors!!!!"
