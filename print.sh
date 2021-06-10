0bbc8b56f6a8e0c48647f437255cd42f68f5a76e
devops7569 <devopstraining7569@gmail.com>
2021-06-10 00:39:48 -0700

    adding "bye"

--- print.sh
+++ print.sh
@@ -13,3 +13,13 @@ echo -e "venkatesh \t \t mounika \n saanvitha"
 echo -e "\e[31mplease check the errors!!!!"
 
 echo "bye"
+
+#echo -e "\e[PROP-COL-CODE;COL-CODEmMESSAGE"
+
+# To disable the color which is enabled then
+
+#echo -e"\e[COL-CODEmMESSAGE\e[0m"
+
+echo -e "\e[31mWarning!! There is an error\e[0m"
+
+echo bye
