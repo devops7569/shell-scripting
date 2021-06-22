3e8923d86da81c30222f884664d5a6c631c75a06
devops7569 <devopstraining7569@gmail.com>
2021-06-22 14:59:54 -0700

    creating exit demo

--- exit.sh
+++ exit.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo Hello
-exit
+exit 1
 echo Bye
 
 ##1 exit command stop the script if it is encountered, No more commands will get exicuted
+##2 simple exit command returns 0 as the exit status
