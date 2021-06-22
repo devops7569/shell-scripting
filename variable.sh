5d9ed7a778f3536aa89d45b0d49076bcbf7e304b
devops7569 <devopstraining7569@gmail.com>
2021-06-22 03:38:21 -0700

    adding "underline"

---
+++ variable.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#variable : if we assign a name to a set of data is called as variable
+
+#Declare a variable in bash shell
+NAME=DEVOPS
+
+#Usually other scripting languages will have data types,but shell by default will not have any data types,all the data is string for shell
+
+Count=2
+
+#Access the variable $VAR_NAME / ${VAR_NAME}
+
+echo NAME=$NAME
+
+FILE1=sample.txt
+FILE2=new.txt
+
+#cp $FILE1 $FILE2
+#if your data is having spaces then use quotes
+NAME="welcome to devops"
+
