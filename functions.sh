a25d1a1bc8ec55b5bea1205e009b9aed3f1d3a53
devops7569 <devopstraining7569@gmail.com>
2021-06-22 16:21:37 -0700

    creating function demo

--- functions.sh
+++ functions.sh
@@ -5,6 +5,8 @@
 #way1
 sample(){
   echo Hello from sample function
+  echo a = $a
+  b = 20
 }
 
 #way2
@@ -13,6 +15,9 @@ sample1(){
   echo Hello from sample 1 function
 }
 
+## Main Program
 ##Access the function
+a = 10
 sample
 sample1
+echo b = $B
