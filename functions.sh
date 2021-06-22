d1341775f2b90860eb300414d8309c2cd5a74481
devops7569 <devopstraining7569@gmail.com>
2021-06-22 15:15:54 -0700

    creating function demo

---
+++ functions.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+##Declare a function
+
+#way1
+sample(){
+  echo Hello from sample function
+}
+
+#way2
+sample1(){
+
+  echop Hello from sample 1 function
+}
+
+##Access the function
+sample
+sample1
